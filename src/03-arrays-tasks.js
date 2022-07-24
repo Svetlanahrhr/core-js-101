55dfa11624e2b7dcca1f2a04b2e3d29c04c49c7f
Svetlanahrhr <maadlana@mail.ru>
2022-07-24 19:16:22 +0400

    feat: add 13 array tasks

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -35,8 +35,12 @@ function findElement(arr, value ) {
  *    2 => [ 1, 3 ]
  *    5 => [ 1, 3, 5, 7, 9 ]
  */
-function generateOdds(/* len */) {
-  throw new Error('Not implemented');
+ function generateOdds(count) {
+  function iter(n, current, acc) {
+    if (n == count) return acc;
+    return iter(n + 1, current + 2, acc.concat(current))
+  }
+  return iter(0, 1, []);
 }
 
 
@@ -52,8 +56,8 @@ function generateOdds(/* len */) {
  *    [0, 1, 2, 3, 4, 5] => [0, 1, 2, 3, 4, 5,   0, 1, 2, 3, 4, 5]
  *    [] => []
  */
-function doubleArray(/* arr */) {
-  throw new Error('Not implemented');
+function doubleArray( arr ) {
+  return arr.concat(arr);
 }
 
 
@@ -68,8 +72,13 @@ function doubleArray(/* arr */) {
  *    [-1, 2, -5, -4, 0] => [ 2 ]
  *    [] => []
  */
-function getArrayOfPositives(/* arr */) {
-  throw new Error('Not implemented');
+ function getArrayOfPositives(arr) {
+  return arr.reduce((acc, item) => {
+    if (item >= 1) {
+      acc.push(item);
+    }
+    return acc;
+  }, []);
 }
 
 /**
@@ -83,8 +92,13 @@ function getArrayOfPositives(/* arr */) {
  *    [ 1, 2, 3, 4, 5 ] => []
  *    [ 'cat, 'dog', 'raccoon' ] => [ 'cat', 'dog', 'raccoon' ]
  */
-function getArrayOfStrings(/* arr */) {
-  throw new Error('Not implemented');
+function getArrayOfStrings( arr ) {
+  return arr.reduce((acc, item) => {
+    if (typeof item == 'string') {
+      acc.push(item);
+    }
+    return acc;
+  }, []);
 }
 
 /**
@@ -100,8 +114,13 @@ function getArrayOfStrings(/* arr */) {
  *    [ 1, 2, 3, 4, 5, 'false' ]         => [ 1, 2, 3, 4, 5, 'false' ]
  *    [ false, 0, NaN, '', undefined ]   => [ ]
  */
-function removeFalsyValues(/* arr */) {
-  throw new Error('Not implemented');
+function removeFalsyValues( arr ) {
+  return arr.reduce((acc, item) => {
+    if (item) {
+      acc.push(item);
+    }
+    return acc;
+  }, []);
 }
 
 /**
@@ -115,8 +134,8 @@ function removeFalsyValues(/* arr */) {
  *    => [ 'PERMANENT-INTERNSHIP', 'GLUTINOUS-SHRIEK', 'MULTIPLICATIVE-ELEVATION' ],
  *    [ 'a', 'b', 'c', 'd', 'e', 'f', 'g' ]  => [ 'A', 'B', 'C', 'D', 'E', 'F', 'G' ]
  */
-function getUpperCaseStrings(/* arr */) {
-  throw new Error('Not implemented');
+function getUpperCaseStrings( arr ) {
+  return arr.map((item) => item.toUpperCase());
 }
 
 
@@ -130,8 +149,8 @@ function getUpperCaseStrings(/* arr */) {
  *    [ '', 'a', 'bc', 'def', 'ghij' ]  => [ 0, 1, 2, 3, 4 ]
  *    [ 'angular', 'react', 'ember' ] => [ 7, 5, 5 ]
  */
-function getStringsLength(/* arr */) {
-  throw new Error('Not implemented');
+function getStringsLength( arr ) {
+  return arr.map((item) => item.length);
 }
 
 /**
@@ -145,8 +164,9 @@ function getStringsLength(/* arr */) {
  *    [ 1, 3, 4, 5 ], 2, 1  => [ 1, 2, 3, 4, 5 ]
  *    [ 1, 'b', 'c'], 0, 'x'  => [ 'x', 1, 'b', 'c' ]
  */
-function insertItem(/* arr, item, index */) {
-  throw new Error('Not implemented');
+ function insertItem(arr, item, index) {
+  arr.splice(index, 0, item);
+  return arr;
 }
 
 /**
@@ -159,8 +179,9 @@ function insertItem(/* arr, item, index */) {
  *    [ 1, 3, 4, 5 ], 2 => [ 1, 3 ]
  *    [ 'a', 'b', 'c', 'd'], 3  => [ 'a', 'b', 'c' ]
  */
-function getHead(/* arr, n */) {
-  throw new Error('Not implemented');
+ function getHead(arr, n) {
+  arr.splice(n);
+  return arr;
 }
 
 
@@ -174,8 +195,9 @@ function getHead(/* arr, n */) {
  *    [ 1, 3, 4, 5 ], 2  => [ 4, 5 ]
  *    [ 'a', 'b', 'c', 'd'], 3  => [ 'b', 'c', 'd' ]
  */
-function getTail(/* arr, n */) {
-  throw new Error('Not implemented');
+function getTail( arr, n ) {
+  arr.splice(0,arr.length-n);
+  return arr;
 }
 
 
@@ -199,8 +221,8 @@ function getTail(/* arr, n */) {
  *    +'20,21,22,23,24\n'
  *    +'30,31,32,33,34'
  */
-function toCsvText(/* arr */) {
-  throw new Error('Not implemented');
+function toCsvText( arr ) {
+  return arr.reduce((acc, curr) => `${String(acc)+'\n'+String(curr)}`)
 }
 
 /**
